089f8b242580f62e0ade1cf3ea417111aba43585
akrampatel89 <akrampatel89@users.noreply.github.com>
2020-07-21 01:42:42 +0530

    Code file created.

---
+++ Telecom-Churn-Prediction-with-Boosting/code.py
@@ -0,0 +1,103 @@
+# --------------
+import pandas as pd
+from sklearn.model_selection import train_test_split
+#path - Path of file 
+
+# Code starts here
+df=pd.read_csv(path)
+
+X=df.drop(['customerID','Churn'], 1).copy()
+y=df.Churn
+
+X_train,X_test,y_train,y_test=train_test_split(X, y, test_size = 0.3, random_state = 0)
+
+
+# --------------
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
+# Code starts here
+X_train['TotalCharges']=X_train.TotalCharges.replace(' ', np.NaN).astype(float)
+
+X_test['TotalCharges']=X_test.TotalCharges.replace(' ', np.NaN).astype(float)
+
+X_train['TotalCharges']=X_train.TotalCharges.fillna(X_train.TotalCharges.mean())
+
+X_test['TotalCharges']=X_test.TotalCharges.fillna(X_test.TotalCharges.mean())
+
+X_train.isna().sum()
+
+lb=LabelEncoder()
+
+for col in list(X_train.select_dtypes(include=object).columns):
+    X_train[col]=lb.fit_transform(X_train[col])
+
+for col in list(X_test.select_dtypes(include=object).columns):
+    X_test[col]=lb.fit_transform(X_test[col])
+
+y_train=y_train.replace({'No':0, 'Yes':1})
+
+y_test=y_test.replace({'No':0, 'Yes':1})
+
+
+
+# --------------
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
+
+# Code starts here
+print(X_train.head(3), '\n', X_test.head(3), '\n', y_train.head(3), '\n', y_test.head(3))
+
+ada_model=AdaBoostClassifier(random_state=0)
+
+ada_model.fit(X_train, y_train)
+
+y_pred=ada_model.predict(X_test)
+
+ada_score=accuracy_score(y_test, y_pred)
+
+ada_cm=confusion_matrix(y_test, y_pred)
+
+ada_cr=classification_report(y_test, y_pred)
+
+print(ada_cm, '\n', ada_cr, '\n', ada_score)
+
+
+# --------------
+from xgboost import XGBClassifier
+from sklearn.model_selection import GridSearchCV
+
+#Parameter list
+parameters={'learning_rate':[0.1,0.15,0.2,0.25,0.3],
+            'max_depth':range(1,3)}
+
+# Code starts here
+xgb_model=XGBClassifier(random_state=0)
+
+xgb_model.fit(X_train, y_train)
+
+y_pred=xgb_model.predict(X_test)
+
+xgb_score=accuracy_score(y_test, y_pred)
+
+xgb_cm=confusion_matrix(y_test, y_pred)
+
+xgb_cr=classification_report(y_test, y_pred)
+
+print('the results for XGB are:', xgb_cm, '\n', xgb_cr, '\n', xgb_score)
+
+clf_model=GridSearchCV(estimator=xgb_model, param_grid=parameters)
+
+clf_model.fit(X_train, y_train)
+
+y_pred=clf_model.predict(X_test)
+
+clf_score=accuracy_score(y_test, y_pred)
+
+clf_cm=confusion_matrix(y_test, y_pred)
+
+clf_cr=classification_report(y_test, y_pred)
+
+print('the results for grid search are:', clf_cm, '\n', clf_cr, '\n', clf_score)
+
+
